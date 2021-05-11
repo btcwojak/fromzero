16bb84a8c6ed0486d281785eeb097a23dfa4c4b3
= <itzsymbaa@outlook.com>
2021-05-11 19:32:00 +0100

    Implemented splash screen

---
+++ app/src/main/java/com/spudg/fromzero/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.spudg.fromzero
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
